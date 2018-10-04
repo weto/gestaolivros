ef110bab6d4c748366df09fbdf6069fc0e477353
Weto <weto.jc@gmail.com>
2018-10-04 17:44:59 -0300

    integração de livraria com carrinho de compra e auditoria

--- dao/PedidoApiDAO.js
+++ dao/PedidoApiDAO.js
@@ -20,9 +20,31 @@ exports.findById = function(pedido) {
 	});
 }
 
+async function autorizacaoCartaoCredito(pedido) {
+	const objetoParam = {
+		numero: pedido.codigo,
+		valor: pedido.valor,
+		pedidos_id: pedido.id, 
+	}
+	const pedidoReturn = await axios.post('http://localhost:5003/api/v1/public/pagamentos', { headers: objetoParam }).then((retorno) => {
+        return retorno.data;
+	})
+	return pedidoReturn;
+}
+
+async function auditoriaTransacao(pedido) {
+	const objetoParam = {
+		idPedidos: pedido.id, 
+	}
+	const auditoriaReturn = await axios.post('http://localhost:3003/api/v1/auditorias/pedidos', { headers: objetoParam }).then((retorno) => {
+        return retorno.data;
+	})
+	return auditoriaReturn;
+}
+
 exports.pagamento = function(carrinho) {
 	return new Promise(function(resolve, reject) {
-		const novoItem = {
+		const novoPedido = {
             "id": ++idDinamico,
             "codigo": "cod ".concat(carrinho.idPedido),
             "situacao": "Aberto",
@@ -32,11 +54,22 @@ exports.pagamento = function(carrinho) {
             }
 		};
 
-		if(gestaoPedido.push(novoItem)) {
-			resolve(gestaoPedido);
-		} else {
-			reject('Erro ao realizar pagamento');
-		}
+		autorizacaoCartaoCredito(novoPedido)
+		.then(autorizacaoReturn => {
+			return autorizacaoReturn;
+		}).then(autorizacao => {
+			auditoriaTransacao(novoPedido).then(autorizacaoReturn => {
+				if(gestaoPedido.push(novoPedido)) {
+					resolve(gestaoPedido);
+				} else {
+					reject('Erro ao realizar pagamento');
+				}
+			}).catch((error) => {
+				reject(error);
+			})
+		}).catch((error) => {
+			reject(error);
+		})
 	});
 }
 
